5b4e67fc85eed4a59edb2f9ccd6ce78c9473bab2
Henrique Vieira <hsfvieira@gmail.com>
2020-12-11 16:18:30 -0300

    feat: add pagination and title search

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="store"> 
-      <Comic v-for="comic in comics" :key="comic.id" :title="comic.title" :image="comic.thumbnail" :price="comic.prices[0].price"></Comic>
-    </div>
-  
+  <input name="buscar" placeholder="Buscar Quadrinho" v-model="comicTitle" />
+  <div class="store"> 
+    <Comic v-for="comic in comics" :key="comic.id" :title="comic.title" :image="comic.thumbnail" :price="comic.prices[0].price"></Comic>
+  </div>
+  <button @click="previousPage()">Voltar</button>
+  <button @click="nextPage()">Próxima</button>
 </template>
 
 <script>
@@ -15,12 +17,32 @@ export default {
   },
   data() {
     return {
-      comics: []
+      comics: [],
+      comicTitle: '',
+      offset: 0
     }
   },
   async mounted() {
-    const { data: { results: comics } } = await getData()
+    const { data: { results: comics } } = await getData(this.comicTitle, this.offset)
     this.comics = comics
+  },
+  watch: {
+    async comicTitle() {
+      const { data: { results: comics } } = await getData(this.comicTitle, this.offset)
+      this.comics = comics
+    }
+  },
+  methods: {
+    async nextPage() {
+      this.offset += 20
+      const { data: { results: comics } } = await getData(this.comicTitle, this.offset)
+      this.comics = comics
+    },
+    async previousPage() {
+      this.offset = this.offset - 20 <= 0 ? 0 : this.offset - 20
+      const { data: { results: comics } } = await getData(this.comicTitle, this.offset)
+      this.comics = comics
+    }
   }
 }
 
